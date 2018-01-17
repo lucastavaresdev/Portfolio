0996f6714187beb3a1fba3ed8440080844369e83
Lucas Tavares <lucas.tavares@ad4pixels.com.br>
2018-01-17 03:53:16 -0200

    .

--- crud/php/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $conn = new PDO('mysql:host=localhost;dbname=banco_teste', 'root', '');
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
-?>
