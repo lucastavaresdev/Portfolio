8746ebdf72738e7964ec44baf0f558306bcbe088
Lucas Tavares <lucastavaresdev@gmail.com>
2018-05-02 10:54:09 -0300

    colocando o serve live

--- iTech/gulpfile.js
+++ iTech/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp  = require('gulp')
-,sass = require('gulp-sass');
+,sass = require('gulp-sass'),
+browserSync = require('browser-sync');
 
 gulp.task('sass', function(){
 gulp.src('./src/sass/**/*.scss')
@@ -7,6 +8,14 @@ gulp.src('./src/sass/**/*.scss')
     .pipe(gulp.dest('./src/css/'));
 })
 
-gulp.task('listen', function(){
-gulp.watch('./src/sass/**/*.scss',['sass'])
-})
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server:{
+            baseDir: 'src'
+        }
+    })
+    gulp.watch('./src/**/*').on('change', browserSync.reload)
+
+    gulp.watch('./src/sass/**/*.scss', ['sass'])
+});
