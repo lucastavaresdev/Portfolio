a33c3637ba2021c59e9133595032d8f473eb7f81
Lucas Tavares <lucastavaresdev@gmail.com>
2018-05-29 14:39:03 -0300

    legenda e titulo do grafico

--- painel/src/js/grafico.js
+++ painel/src/js/grafico.js
@@ -1,16 +1,12 @@
 var ctx = document.getElementById("graficoPacientes");
-var ctx = document.getElementById("graficoPacientes").getContext("2d");
-var ctx = $("#graficoPacientes");
-var ctx = "graficoPacientes";
 
-var ctx = document.getElementById("graficoPacientes");
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
         labels: ["Aguardando", "Em Atendimento", "Indisponivel", "Finalizado"],
         datasets: [{
             //label: '# of Votes',
-            data: [12, 19, 3,3],
+            data: [12, 19, 3, 3],
             backgroundColor: [
                 'rgba(22, 159, 133, 0.2)',
                 'rgba(42, 128, 185, 0.2)',
@@ -27,4 +23,13 @@ var myChart = new Chart(ctx, {
             borderWidth: 1
         }]
     },
+    options: {
+        title: {
+            display: true,
+            text: 'Atendimento de Pacientes',
+        },
+        legend: {
+            position: 'right'
+        }
+    }
 });
