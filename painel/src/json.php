4692a3a6a09f79b9b826b793687c04e6b565442e
Lucas Tavares <lucastavaresdev@gmail.com>
2018-05-30 16:19:08 -0300

    recomeçando a logica

--- painel/src/json.php
+++ painel/src/json.php
@@ -23,54 +23,10 @@
 <body>
 </body>
 
-<?php 
-
-    require('php/selectsJson.php');
-
-
-    $sql = 'SELECT * FROM agendamento';
-
-    $agendamento  = query($sql);
-    echo $agendamento;
-    
-    
-    $sql = 'SELECT * FROM teste';
-    $teste = query($sql);
-
-    echo $teste;
-
-?>
 
 
 <script src="componentes/jquery.js"></script>
-<script>
-
-
-    
-function tabela(urlJson ){
-    $(document).ready(function(){
-        $('#tabela').empty(); //Limpando a tabela
-        $.ajax({
-            type:'post',		//Definimos o método HTTP usado
-            dataType: 'json',	//Definimos o tipo de retorno
-            url: urlJson ,//Definindo o arquivo onde serão buscados os dados
-            success: function(result){
-                for(var i=0;result.length>i;i++){
-                    $('#tabela').append('<tr><td>'+result[i].id+'</td><td>'+result[i].nm_paciente+'</td>');
-                }
-            }
-        });
-    });
-}
-
-var urlJson = './php/selectsJson.php';
-tabela(urlJson)
-
-
-
-
-
-
+<script src="componentes/requisicoes.js"></script>
 </script>
 
 </html>
