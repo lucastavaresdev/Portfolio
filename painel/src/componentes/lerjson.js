2024f01e77b9042ab089f632577c9add636f25ed
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-01 14:00:07 -0300

    ajax funcionando no console.log

--- painel/src/componentes/lerjson.js
+++ painel/src/componentes/lerjson.js
@@ -3,10 +3,14 @@ $.ajax({
     dataType: "json", // tipo de arquivo
     url: 'php/selectsJson.php',// local do json
     data: 'linha', // linha
-    success: function(data){//se funcionar execulta essa função
+    success: function (data) {//se funcionar execulta essa função
         console.log(data);
-    }
-  });
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i]['id']);
+            console.log(data[i]['nm_paciente']);
+        }//fim loop
+    }//fim funcao
+});//fim do ajax
 
 
 
