7e230cc293014006c55088fde94f7f3ac795888a
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-01 17:29:54 -0300

    resetando tabela teste

--- painel/src/componentes/lerjson.js
+++ painel/src/componentes/lerjson.js
@@ -1,21 +1,21 @@
 
-$.ajax({
-    dataType: "json", // tipo de arquivo
-    url: 'php/selectsJson.php',// local do json
-    data: 'linha', // linha
-    success: function (data) {//se funcionar execulta essa função
-        console.log(data);
+
+function listapacientes() {
+    $.ajax({
+        dataType: "json", // tipo de arquivo
+        url: 'php/selectsJson.php',// local do json
+        data: 'linha', // linha
+        success: function (data) {//se funcionar execulta essa função
             for (let i = 0; i < data.length; i++) {
-            //console.log(data[i]['nm_paciente']);
-                var table = document.querySelector("tbody");
-                var row = table.insertRow(0);
-                var id = row.insertCell(0);
-                var nome = row.insertCell(1);
-                id.innerHTML = data[i]['id'];
-                nome.innerHTML = data[i]['nm_paciente'];
-        }//fim loop
-    }//fim funcao
-});//fim do ajax
+                $('#tabela').append('<tr id="linha"><td>' + data[i]['id'] + '</td><td>' + data[i]['nm_paciente'] + '</td><td>');
+            }//fim loop
+        }//fim funcao
+    });//fim do ajax
+}
 
+setInterval(function atualiza() {
+    document.getElementById("tabela").innerHTML=""
+    listapacientes();
+}, 100000);
 
-
+listapacientes();
