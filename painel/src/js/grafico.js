f5f0197500e63b71595887bbf51bb032b10dc6b8
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-04 15:16:56 -0300

    grafico recebendo gerado pelo banco ok

--- painel/src/js/grafico.js
+++ painel/src/js/grafico.js
@@ -1,47 +1,51 @@
 chamadaAjax('php/selectsJson.php?parametro=qtdstatus', exibir);
 
 
-function exibir(data){
-    
-    console.log(data[0]['status'] + data[0]['qtdStatus'])
-}
-
-
-var ctx = document.getElementById("graficoPacientes");
-
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ["Aguardando", "Em Atendimento", "Indisponivel", "Finalizado"],
-        datasets: [{
-            //label: '# of Votes',	
-            data: [12, 19, 3, 3],
-            backgroundColor: [
-                'rgba(22, 159, 133, 1)',
-                'rgba(42, 128, 185, 1)',
-                'rgba(143, 68, 173, 1)',
-                'rgba(86, 86, 86, 1)',
-
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Atendimento de Pacientes',
-        },
-        legend: {
-            position: 'bottom',
-            fullWidth: false,
-            labels: {
-                fontSize: 12,
-                boxWidth: 15
-            }
-        },
-        layout: {
-            responsive: true
-        }
+function exibir(data) {
+    arrNomesStatus = [];
+    arrQuantidadeStatus = [];
+    for (let i = 0; i < data.length; i++) {
+        arrNomesStatus.push(data[i]['status']);
+        arrQuantidadeStatus.push(data[i]['qtdStatus']);
     }
-});
 
+    var ctx = document.getElementById("graficoPacientes");
+
+//o ideial é a cor vir do banco de dados
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: arrNomesStatus,
+            datasets: [{
+                //label: '# of Votes',	
+                data: arrQuantidadeStatus,
+                backgroundColor: [
+                    'rgba(22, 159, 133, 1)',
+                    'rgba(42, 128, 185, 1)',
+                    'rgba(86, 86, 86, 1)',
+                    'rgba(143, 68, 173, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Atendimento de Pacientes',
+            },
+            legend: {
+                position: 'bottom',
+                fullWidth: false,
+                labels: {
+                    fontSize: 12,
+                    boxWidth: 15
+                }
+            },
+            layout: {
+                responsive: true
+            }
+        }
+    });
+
+
+}
