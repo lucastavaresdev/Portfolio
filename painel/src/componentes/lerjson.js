b82885df4877d274558fff5d51433ab77cdddca2
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-08 11:01:00 -0300

    tabelas com dados do banco finalizada

--- painel/src/componentes/lerjson.js
+++ painel/src/componentes/lerjson.js
@@ -9,22 +9,35 @@ function localizacao(data) {
 
 setInterval(function () {
     chamadaAjax('php/selectsJson.php?parametro=localizacao', tabela);
-    //chamadaAjax('php/selectsJson.php?parametro=localizacao', localizacao);
+    chamadaAjax('php/selectsJson.php?parametro=listasetores', localizacao);
 },1000);
 
+
+
+
+arr=[]
+arr = chamadaAjax('php/selectsJson.php?parametro=localizacao', retornodata);
+
+function retornodata(data){
+    return data;
+}
 
 
 function tabela_localizacao(data, id_da_tabela, html) {
     var html = "";
     
     for (let i = 0; i < data.length; i++) {
-        html += '<tr class="linha_pacientes"><td>' + data[i].paciente + '</td><td class="status"  class="' + data[i].setor + '">' + data[i].setor + '</td>';    
+        html += '<tr class="linha_pacientes"><td>' + data[i].nome + '</td><td class="status"  class="' + data[i].permanencia + '">' + data[i].permanencia + '</td>';    
     }
 
     document.getElementById("tabela_localizacao").innerHTML = html;
     var status = document.querySelectorAll(".status_localizacao");
     coresStatus(status);
 }
+
+
+
+
 
 
 //rastrio de pacientes
@@ -73,9 +86,3 @@ function totaldePaciente() {
 function exibirTotalPaciente(data) {
     var numTotal = document.querySelector('#totaldePacientes').textContent = data[0]['totpacientes'];;
 }
-
-
-//<p>Paciente:<span class="dados-emuso"> Vinícius Cardoso Rocha</span></p><br>
-  //                                          <p>Data de Nascimento:<span class="dados-emuso">25/05/1980</span></p><br>
-    ///                                       <p>Procedimento:<span class="dados-emuso">Consulta</span></p><br>
-                                           //<p>Tempo:<span class="dados-emuso">1:31</span></p><br>
