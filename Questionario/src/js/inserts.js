c645c9c61b37fda375e8606d96eaefcfc164f992
Lucas Tavares <lucastavaresdev@gmail.com>
2018-07-02 15:46:51 -0300

    iniciando a home

--- Questionario/src/js/inserts.js
+++ Questionario/src/js/inserts.js
@@ -1,4 +1,5 @@
 var refereciaServidor = 'http://192.168.1.102/z/'
 
 //cadastro
-document.getElementById("cadastroUsuario").action = refereciaServidor;
+document.getElementById("cadastroUsuario").action = refereciaServidor + "inserts.php?parametro=cadastroUsuario";
+
