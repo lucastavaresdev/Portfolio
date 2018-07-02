b09704954f2c89d10a4d1a2e1cde03ee9d4d67b8
Lucas Tavares <lucastavaresdev@gmail.com>
2018-07-02 11:39:09 -0300

    login sem hash

---
+++ Questionario/src/js/conexaophp.js
@@ -0,0 +1,9 @@
+// // var refereciaServidor = 'https://swapi.co/api/films/2/'
+
+// chamadaAjax(refereciaServidor, teste);
+// // chamadaAjax(refereciaServidor + 'starwars.json', teste);
+
+// function teste(data){
+//     console.log(data.title);
+// }
+
