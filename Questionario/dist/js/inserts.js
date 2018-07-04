6a24eb1ac12b97f8f5c5c3435abdc3a8416e19df
Lucas Tavares <lucastavaresdev@gmail.com>
2018-07-04 10:52:48 -0300

    insert cadastro do questionario titulo

--- Questionario/dist/js/inserts.js
+++ Questionario/dist/js/inserts.js
@@ -1,5 +1,7 @@
 var refereciaServidor = 'http://192.168.1.102/z/'
 
-//cadastro
+//cadastro 
 document.getElementById("cadastroUsuario").action = refereciaServidor + "inserts.php?parametro=cadastroUsuario";
 
+
+document.getElementById("cadastroQuestionario").action = refereciaServidor + "inserts.php?parametro=cadastroQuestionario";
