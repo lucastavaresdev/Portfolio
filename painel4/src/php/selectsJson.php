d332bce771e5d1f21a242e2cb01738a263a87a7d
Lucas Tavares <lucastavaresdev@gmail.com>
2018-07-11 11:12:40 -0300

    selects dos intevalos por hora do dia

--- painel4/src/php/selectsJson.php
+++ painel4/src/php/selectsJson.php
@@ -8,11 +8,20 @@ $parametro =$_GET['parametro'];//PARAMETRO
 $agendamentos_do_dia = "SELECT count(distinct(nome_paciente)) as agendamento_do_dia
 FROM agendamento where STR_TO_DATE(data_servico_atual, '%d/%m/%Y') =  CURDATE()";
 
+$lista_dos_intevalos_por_hora_do_dia = "SELECT  CONCAT(HOUR(hora_servico_selecionado), ':00-', HOUR(hora_servico_selecionado)+1, ':00')  intervalo_de_horas, COUNT(*) as `usage`
+FROM agendamento
+where STR_TO_DATE(data_servico_atual, '%d/%m/%Y') =  CURDATE()
+GROUP BY HOUR(hora_servico_selecionado)";
+
 
 //parametro passado
 if($parametro === 'agendamentos_do_dia'){
   geraJson($agendamentos_do_dia , $conexao );
+}else if($parametro === 'lista_dos_intevalos_por_hora_do_dia'){
+  geraJson($lista_dos_intevalos_por_hora_do_dia, $conexao);
 }
+ 
+
  
 
 //retorna e exibe o json
