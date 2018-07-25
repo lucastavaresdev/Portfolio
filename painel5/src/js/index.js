06b2d3b2150cc96a9fe7e01ca5ca1d25f8b0a890
Lucas Tavares <lucastavaresdev@gmail.com>
2018-07-25 10:21:31 -0300

    datatable na tabela

--- painel5/src/js/index.js
+++ painel5/src/js/index.js
@@ -14,28 +14,6 @@
 
 
 
-function lista_de_pacientes(data) {
-    console.log(data);
-    var tbody = document.getElementById("listadePacientes");
-    if (tbody) {
-        for (i = 0; i < data.length; i++) {
-            var tr = document.createElement('tr');
-
-            var cols =
-                '<td>' + data[i].id + '</td>'
-                + '<td>' + data[i].nm_paciente + '</td>'
-                + '<td>' + data[i].Hora_cirurgia + '</td>'
-                + '<td>' + data[i].Cirurgia + '</td>'
-                + '<td>' + data[i].Cirurgiao + '</td>'
-                + '<td>' + data[i].Centro_Cirurgico + '</td>'
-                + '<td>' + data[i].Observacao + '</td>'
-
-            var linha = tr.innerHTML = cols;
-            tbody.innerHTML += linha;
-        }
-        //         data_table()
-    }
-}
 
 
 function total_de_pacientes(data) {
@@ -59,7 +37,49 @@ function total_de_pacientes(data) {
 
 
 
+function lista_de_pacientes(data) {
+    console.log(data);
+    var tbody = document.getElementById("listadePacientes");
+    if (tbody) {
+        for (i = 0; i < data.length; i++) {
+            var tr = document.createElement('tr');
 
+            var cols =
+                '<td>' + data[i].id + '</td>'
+                + '<td>' + data[i].nm_paciente + '</td>'
+                + '<td>' + data[i].Hora_cirurgia + '</td>'
+                + '<td>' + data[i].Cirurgia + '</td>'
+                + '<td>' + data[i].Cirurgiao + '</td>'
+                + '<td>' + data[i].Centro_Cirurgico + '</td>'
+                + '<td>' + data[i].Observacao + '</td>'
+
+            var linha = tr.innerHTML = cols;
+            tbody.innerHTML += linha;
+        }
+        data_table()
+    }
+}
+
+function data_table() {
+    $(document).ready(function () {
+        $('#tabela_pacientes').DataTable({
+            language: {
+                "lengthMenu": " Quantidade por Pagina _MENU_  ",
+                "zeroRecords": "Não encontrado pacientes",
+                "info": "Total de Pagina _PAGE_ de _PAGES_",
+                "infoEmpty": " ",
+                "infoFiltered": "(filtered from _MAX_ total records)",
+                "search": "Filtrar:",
+                "paginate": {
+                    "first": " ",
+                    "next": "Proxima",
+                    "previous": "Anterior",
+                    "last": " "
+                }
+            }
+        });
+    });
+}
 
 
 
@@ -96,32 +116,12 @@ function total_de_pacientes(data) {
 //     elem.innerHTML = qtd_procedimentos;
 
 
-
 // }
 
 
 
 
-// function data_table() {
-//     $(document).ready(function () {
-//         $('#tabela_pacientes').DataTable({
-//             language: {
-//                 "lengthMenu": " Quantidade por Pagina _MENU_  ",
-//                 "zeroRecords": "Não encontrado pacientes",
-//                 "info": "Total de Pagina _PAGE_ de _PAGES_",
-//                 "infoEmpty": " ",
-//                 "infoFiltered": "(filtered from _MAX_ total records)",
-//                 "search": "Filtrar:",
-//                 "paginate": {
-//                     "first": " ",
-//                     "next": "Proxima",
-//                     "previous": "Anterior",
-//                     "last": " "
-//                 }
-//             }
-//         });
-//     });
-// }
+
 
 
 
